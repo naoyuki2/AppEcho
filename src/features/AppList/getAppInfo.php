9e47132480d852881cbcfd11ba02887b1fc792c6
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-05-07 11:36:20 +0900

    アプリ一覧画面の作成

--- src/features/AppList/getAppInfo.php
+++ src/features/AppList/getAppInfo.php
@@ -1,14 +1,14 @@
 <?php
-    require_once '../../config/db-connect.php';
-    
-    function getAppList() {
-        global $pdo;
-        $sql = $pdo->query('
+require_once '/home/users/2/verse.jp-aso2201030/web/AppEcho/config/db_connect.php';
+
+function getAppList()
+{
+    global $pdo;
+    $sql = $pdo->query('
             SELECT app.id, app.name, app.image_url, app.keyword, category.name AS category_name
-            FROM app 
-            INNER JOIN category 
+            FROM app
+            INNER JOIN category
             ON app.category_id = category.id
         ');
-        return $sql->fetchAll();
-    }
-?>
+    return $sql->fetchAll();
+}
--- src/pages/AppList.php
+++ src/pages/AppList.php
@@ -1,14 +1,25 @@
 <?php
-    require_once '../components/Header.php';
-    require_once '../features/AppList/getAppInfo.php';
-    $AppList = getAppList();
+require_once '../components/Header.php';
+require_once '../features/AppList/getAppInfo.php';
+$AppList = getAppList();
 ?>
 
-<?php foreach ($AppList as $app) { ?>
-    <img src="<?php echo $app['image_url'] ?>" alt="">
-    <p><?php echo $app['name'] ?></p>
-<?php } ?>
+<div class="AppList_wrap">
+
+    <?php
+    $cnt = 0;
+    foreach ($AppList as $app) {
+    ?>
+        <div class="AppList_content">
+            <img class="AppList_img" src="<?php echo $app['image_url'] ?>" alt="<?php echo $app['name'] ?>">
+            <p class="AppList_p"><?php echo $app['name'] ?></p>
+        </div>
+    <?php
+    }
+    ?>
+
+</div>
 
 <?php
-    require_once '../components/Footer.php';
+require_once '../components/Footer.php';
 ?>
