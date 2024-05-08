94b34264a94b4784517c8fdaae6bd2fc2e47ebe0
aespakari7 <147119919+aespakari7@users.noreply.github.com>
2024-05-08 11:39:09 +0900

    Header.phpとcss(途中)

--- src/components/Header.php
+++ src/components/Header.php
@@ -7,5 +7,17 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/AppList.css">
         <link rel="stylesheet" href="../css/AppReviews.css">
+        <link rel="stylesheet" href="../css/Header.css">
     </head>
     <body>
+
+        <div class="Header-logo">
+            <img src="ロゴ 1.png">
+        </div>
+
+        <div class="Header-search">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+
+    </body>
+</html>
