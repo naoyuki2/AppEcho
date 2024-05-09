ddc4ce8d1b80c14e24bf0bfee562b39f81094103
aespakari7 <147119919+aespakari7@users.noreply.github.com>
2024-05-09 10:15:04 +0900

    Header

--- src/components/Header.php
+++ src/components/Header.php
@@ -5,18 +5,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title> </title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
         <link rel="stylesheet" href="../css/AppList.css">
         <link rel="stylesheet" href="../css/AppReviews.css">
         <link rel="stylesheet" href="../css/Header.css">
     </head>
-    <body>
-
+    <body> 
         <div class="Header-logo">
-            <img src="logo.png">
+            <img src="../../img/logo.png">
         </div>
 
         <div class="Header-search">
-            <i class="fa-solid fa-magnifying-glass"></i>
+                <i class="fa-solid fa-magnifying-glass"></i>
         </div>
 
     </body>
