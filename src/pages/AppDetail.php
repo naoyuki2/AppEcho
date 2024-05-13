8e8bdfbe10089741e5a792070fdbea7b4392ee23
1901004 <166771485+1901004@users.noreply.github.com>
2024-05-13 10:27:12 +0900

    AppDetail作成

--- src/pages/AppDetail.php
+++ src/pages/AppDetail.php
@@ -1,15 +1,69 @@
 <?php
 require_once '../components/Header.php';
-require_once '../features/AppDetail/getAppDetail.php';
-$AppList = getAppDetail();
+//require_once '../features/AppDetail/getAppDetail.php';
+//$AppList = getAppDetail();
 ?>
 
 <?php
-echo '<pre>';
-print_r($AppList);
-echo '</pre>';
+//echo '<pre>';
+//print_r($AppList);
+//echo '</pre>';
 ?>
 
+<div class="AppDetail-main">
+    <div class="AppDetail-pad">
+
+        <div align="center">
+            <img src="" alt="" class="AppDetail-ex">
+        </div>
+
+        <div align="left">
+            <b><p class="AppDetail-midasi">アプリ名</p></b>
+        </div>
+        <div align="center">
+            <p>appname</p>
+        </div>
+
+        <div align="left">
+            <b><p class="AppDetail-midasi">アプリ説明</p></b>
+        </div>
+        <div align="center">
+            <p>appnaiyou</p>
+        </div>
+
+        <div align="left">
+            <b><p class="AppDetail-midasi">ダウンロード</p></b>
+        </div>
+        <div align="center">
+            <img src="../img/App_Store_Badge_JP_blk_100317.png" alt="" width=120>
+            <img src="../img/google-play-badge.png" alt="" width=155>
+        </div>
+
+        <div align="left">
+            <b><p class="AppDetail-midasi">平均評価</p></b>
+        </div>
+        <div align="center">
+            <i class="fa-solid fa-star fa-3x" style="color: #FFD43B;"></i>
+            <i class="fa-regular fa-star fa-3x" style="color: #4b4b4b"></i>
+            <i class="fa-regular fa-star fa-3x" style="color: #4b4b4b"></i>
+            <i class="fa-regular fa-star fa-3x" style="color: #4b4b4b"></i>
+            <i class="fa-regular fa-star fa-3x" style="color: #4b4b4b"></i>
+        </div>
+
+        <div class="AppDetail-foot">
+            <div class="AppDetail-fl-left">
+                <p class="AppDetail-com">100</p>
+                <i class="fa-regular fa-comment fa-3x" style="color: #4b4b4b;"></i>
+            </div>
+            <div class="AppDetail-fl-right">
+            <div class="AppDetail-pad-botm">
+                <button class="AppDetail-categori">カテゴリ</button>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
 <?php
-require_once '../components/Footer.php';
+    require_once '../components/Footer.php';
 ?>
