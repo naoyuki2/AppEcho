9e29f42e4e418e4aa1f6620666a9e3fc1ddcde4b
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-05-14 11:18:51 +0900

    getAppDetailの変更

--- src/features/AppDetail/getAppDetail.php
+++ src/features/AppDetail/getAppDetail.php
@@ -1,17 +1,20 @@
 <?php
 require_once '/home/users/2/verse.jp-aso2201030/web/AppEcho/config/db_connect.php';
 
-function getAppDetail()
+function getAppDetail($appId)
 {
     global $pdo;
-    $sql = $pdo->query('
+    $sql = $pdo->prepare('
             SELECT app.id, app.name, app.image_url, app.info, app.appLink, app.playLink, app.upload_date, category.name AS category_name, COUNT(review.id) AS review, ROUND(AVG(review.star)) AS star
             FROM app
             INNER JOIN category
             ON app.category_id = category.id
             LEFT JOIN review
             ON app.id = review.app_id
-            WHERE app.id = 1
+            WHERE app.id = ?
         ');
+
+    $sql->execute([$appId]);
+
     return $sql->fetchAll();
 }
