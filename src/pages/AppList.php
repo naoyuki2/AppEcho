e5874ca91ed16e9c04421b59af0c3726d3341c4b
1901004 <1901004@s.asojuku.ac.jp>
2024-05-14 11:02:23 +0900

    遷移するようにした

--- src/pages/AppList.php
+++ src/pages/AppList.php
@@ -3,14 +3,13 @@ require_once '../components/Header.php';
 require_once '../features/AppList/getAppInfo.php';
 $AppList = getAppList();
 ?>
-
 <div class="AppList_wrap">
 
     <?php
     foreach ($AppList as $app) {
     ?>
         <div class="AppList_content">
-            <a href="#"><img class="AppList_img" src="<?php echo $app['image_url'] ?>" alt="<?php echo $app['name'] ?>"></a>
+            <a href="../AppDetail.php?<?php echo $app['id']?>"><img class="AppList_img" src="<?php echo $app['image_url'] ?>" alt="<?php echo $app['name'] ?>"></a>
             <p class="AppList_p"><?php echo $app['name'] ?></p>
         </div>
     <?php
