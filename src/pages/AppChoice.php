b0437893294d116a5281361cb3f713465aa2f532
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-05-21 09:53:29 +0900

    AppChoice.phpの作成
    検索画面から一覧画面への値の受け渡しをしています

---
+++ src/pages/AppChoice.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../features/AppSearch/AppSearch.php';
+
+if (!empty($_POST['AppName'])) {
+    $text = $_POST['AppName'];
+} else {
+    $text = 0;
+}
+
+if (!empty($_POST['star'])) {
+    $star = $_POST['star'];
+} else {
+    $star = 0;
+}
+
+if (!empty($_POST['category'])) {
+    $category = $_POST['category'];
+} else {
+    $category = 0;
+}
+
+$appId = getAppSearch($text, $category, $star);
+
+if ($appId != -1 && $appId != 0) {
+    $queryString = http_build_query($appId);
+} else if ($appId == -1) {
+    $queryString = http_build_query(['app' => -1]);
+} else {
+    $queryString = http_build_query(['app' => 0]);
+}
+
+header('Location: ./AppList.php?'. $queryString);
