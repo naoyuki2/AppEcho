9a4ad3ab09ad204dd4c36d5ec8ca9a3c629ffc7f
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-05-28 10:47:37 +0900

    getTagですべてのカラムを取ってくるように修正

--- src/features/AppFilter/getTag.php
+++ src/features/AppFilter/getTag.php
@@ -4,7 +4,7 @@ require_once '../../config/db_connect.php';
     function getTag(){
         global $pdo;
         $sql = $pdo->query('
-            SELECT tag.id, tag.name
+            SELECT *
             FROM tag
         ');
         return $sql->fetchAll();
