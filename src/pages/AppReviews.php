0d11d9dbb464493d728e5cb288e728bed2fae009
aespakari7 <147119919+aespakari7@users.noreply.github.com>
2024-05-28 10:04:09 +0900

    postReview.php完成

--- src/pages/AppReviews.php
+++ src/pages/AppReviews.php
@@ -1,7 +1,8 @@
 <?php
     require_once '../components/Header.php';
     require_once '../features/AppReviews/getReviews.php';
-    $Reviews = getReviews($_GET['appId']);
+    $appId = $_GET['appId'];
+    $Reviews = getReviews($appId);
 ?>
 
 <?php foreach($Reviews as $review){ ?>
@@ -29,6 +30,8 @@
 </button>
 
 <!-- モーダルの内容 -->
+<form action="../features/AppReviews/postReview.php?appId=<?php echo $appId; ?>" method="post">
+
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -39,15 +42,15 @@
       <div class="modal-body">
         <div class="AppReviews-subtitle"><h4>評価</h4></div>
         <div class="AppReviews-modal-star" id="modal-star">
-            <input type="radio" class="AppReviews-radio-star" name="star" id="AppReviews-star-1">
+            <input value="1" type="radio" class="AppReviews-radio-star" name="star" id="AppReviews-star-1">
             <label for="AppReviews-star-1" class="AppReviews-radio-label" id=1 onclick="toggleClass(0)"><i class="far fa-star"></i>1</label>
-            <input type="radio" class="AppReviews-radio-star" name="star" id="AppReviews-star-2">
+            <input value="2" type="radio" class="AppReviews-radio-star" name="star" id="AppReviews-star-2">
             <label for="AppReviews-star-2" class="AppReviews-radio-label" id=2 onclick="toggleClass(1)"><i class="far fa-star"></i>2</label>
-            <input type="radio" class="AppReviews-radio-star" name="star" id="AppReviews-star-3">
+            <input value="3" type="radio" class="AppReviews-radio-star" name="star" id="AppReviews-star-3">
             <label for="AppReviews-star-3" class="AppReviews-radio-label" id=3 onclick="toggleClass(2)"><i class="far fa-star"></i>3</label>
-            <input type="radio" class="AppReviews-radio-star" name="star" id="AppReviews-star-4">
+            <input value="4" type="radio" class="AppReviews-radio-star" name="star" id="AppReviews-star-4">
             <label for="AppReviews-star-4" class="AppReviews-radio-label" id=4 onclick="toggleClass(3)"><i class="far fa-star"></i>4</label>
-            <input type="radio" class="AppReviews-radio-star" name="star" id="AppReviews-star-5">
+            <input value="5" type="radio" class="AppReviews-radio-star" name="star" id="AppReviews-star-5">
             <label for="AppReviews-star-5" class="AppReviews-radio-label" id=5 onclick="toggleClass(4)"><i class="far fa-star"></i>5</label>
         </div>
         <div class="AppReviews-subtitle"><h4>タグ</h4></div>
@@ -62,14 +65,15 @@
             <label for="AppReviews-answer" class="AppReviews-radio-label-answer">回答</label>
         </div>
         <div class="AppReviews-subtitle"><h4>レビュー</h4></div>
-        <div><input type="text" class="AppReviews-review" name="review" id="" placeholder="入力してください"></div>
+        <div><input type="text" class="AppReviews-review" name="content" id="" placeholder="入力してください"></div>
       </div>
       <div class="modal-footer">
-        <button type="button" class="AppReviews-post" data-bs-dismiss="modal" aria-label="Close">投稿する</button>
+        <button type="submit" class="AppReviews-post" data-bs-dismiss="modal" aria-label="Close">投稿する</button>
       </div>
     </div>
   </div>
 </div>
+</form>
 
 <script src="../features/AppReviews/AppReviews-modal-star.js"></script>
 
