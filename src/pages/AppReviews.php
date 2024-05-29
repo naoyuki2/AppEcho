d247f99eacfc2924f555e9fe6a41ff8ef2a3b5ad
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2024-05-29 11:43:13 +0900

    改行するための関数を追加

--- src/pages/AppReviews.php
+++ src/pages/AppReviews.php
@@ -44,7 +44,7 @@
             <?php echo $review['tag_name'] ?>
         </span>
         <p class="AppReviews-comment">
-            <?php echo $review['content'] ?>
+            <?php echo nl2br($review['content']) ?>
         </p>
         <div class="AppReviews-date">
             <?php echo $review['post_date'] ?>
