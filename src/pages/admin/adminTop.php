742f6cdf98f39a276cfc6c42bf39176d9c6bdb2b
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-06-10 10:59:00 +0900

    ログアウトページへのリンクを追加

--- src/pages/admin/adminTop.php
+++ src/pages/admin/adminTop.php
@@ -5,7 +5,7 @@
 <a href = "#">投稿</a>
 <a href = "./CategoryList.php">カテゴリ一覧</a>
 <a href = "#">タグ一覧</a>
-<a href = "#">ログアウト</a>
+<a href = "../../features/auth/admin.php?id=2">ログアウト</a>
 
 <?php
     require_once dirname(__FILE__, 3) . '/components/Footer.php';
