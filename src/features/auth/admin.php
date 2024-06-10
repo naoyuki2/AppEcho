a11b27c20c29dfb1ef4ad4c93d0fe0fdcafa3258
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-06-10 10:59:28 +0900

    ログアウト処理の追加

--- src/features/auth/admin.php
+++ src/features/auth/admin.php
@@ -2,24 +2,41 @@
 session_start();
 require_once dirname(__FILE__, 4) . '/config/db_connect.php';
 
-global $pdo;
-$sql = $pdo->query('
-    SELECT *
-    FROM admin
-    WHERE id = 1
-');
-$adminList = $sql->fetchAll();
-
-$password = $_POST['password'];
-
-foreach ($adminList as $admin) {
-    $hashPassword = $password. $admin['salt'];
-
-    if (password_verify($hashPassword, $admin['hashed_password'])) {
-        $_SESSION['admin'] = true;
-        header('Location: ../../pages/admin/adminTop.php');
-    } else {
-        $_SESSION['admin'] = false;
-        header('Location: ../../pages/admin/login.php');
+if (isset($_GET['id'])) {
+    if ($_GET['id'] == 1) {
+        login();
+    } else if ($_GET['id'] == 2) {
+        logout();
     }
+}
+
+function login()
+{
+    global $pdo;
+    $sql = $pdo->query('
+        SELECT *
+        FROM admin
+        WHERE id = 1
+    ');
+    $adminList = $sql->fetchAll();
+
+    $password = $_POST['password'];
+
+    foreach ($adminList as $admin) {
+        $hashPassword = $password . $admin['salt'];
+
+        if (password_verify($hashPassword, $admin['hashed_password'])) {
+            $_SESSION['admin'] = true;
+            header('Location: ../../pages/admin/adminTop.php');
+        } else {
+            $_SESSION['admin'] = false;
+            header('Location: ../../pages/admin/login.php');
+        }
+    }
+}
+
+function logout()
+{
+    unset($_SESSION['admin']);
+    header('Location: ../../pages/user/AppList.php');
 }
