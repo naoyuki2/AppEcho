4f1a1a6907b01e756d661e3863dbf8f69259705a
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-06-19 11:42:28 +0900

    Auth.phpの更新

--- src/pages/user/Auth.php
+++ src/pages/user/Auth.php
@@ -1,17 +1,20 @@
-<?php require_once dirname(__FILE__, 3) . '/components/Header.php' ?>
+<?php
+require_once dirname(__FILE__, 3) . '/components/Header.php';
+$isSignUp = isset($_GET['signUp']) && $_GET['signUp'] == true;
+?>
 
 <div class="Auth-wrap">
     <ul class="nav nav-tabs nav-fill Auth-nav" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="login-tab" data-bs-toggle="tab" data-bs-target="#login-tab-pane" type="button" role="tab" aria-controls="login-tab-pane" aria-selected="true">ログイン</button>
+            <button class="nav-link <?php echo $isSignUp ? '' : 'active' ?>" id="login-tab" data-bs-toggle="tab" data-bs-target="#login-tab-pane" type="button" role="tab" aria-controls="login-tab-pane" aria-selected="true">ログイン</button>
         </li>
         <li class="nav-item" role="presentation">
-            <button class="nav-link" id="signUp-tab" data-bs-toggle="tab" data-bs-target="#signUp-tab-pane" type="button" role="tab" aria-controls="signUp-tab-pane" aria-selected="false">新規登録</button>
+            <button class="nav-link <?php echo $isSignUp ? 'active' : '' ?>" id="signUp-tab" data-bs-toggle="tab" data-bs-target="#signUp-tab-pane" type="button" role="tab" aria-controls="signUp-tab-pane" aria-selected="false">新規登録</button>
         </li>
     </ul>
 
     <div class="tab-content Auth-tab-content">
-        <div class="tab-pane fade show active" id="login-tab-pane" role="tabpanel" aria-labelledby="login-tab-pane" tabindex="0">
+        <div class="tab-pane fade<?php echo $isSignUp ? '' : 'show active' ?>" id="login-tab-pane" role="tabpanel" aria-labelledby="login-tab-pane" tabindex="0">
             <form action="#" method="post" name="login">
                 <div class="Auth-inputContent">
                     <p class="Auth-label">メールアドレス</p>
@@ -28,25 +31,32 @@
             </form>
         </div>
 
-        <div class="tab-pane fade" id="signUp-tab-pane" role="tabpanel" aria-labelledby="signUp-tab-pane" tabindex="0">
-            <form action="#" method="post" name="signUp">
+        <div class="tab-pane fade <?php echo $isSignUp ? 'show active' : '' ?>" id="signUp-tab-pane" role="tabpanel" aria-labelledby="signUp-tab-pane" tabindex="0">
+            <form action="../../features/auth/SignUp.php" method="post" name="signUp">
                 <div class="Auth-inputContent">
                     <p class="Auth-label">ユーザーネーム</p>
-                    <input type="text" name="text" class="Auth-text Auth-signUpInput" id="Auth-signUp-name" size="30" placeholder="20文字以内">
+                    <input type="text" name="signUpUserName" class="Auth-text Auth-signUpInput" id="Auth-signUp-name" size="30" placeholder="20文字以内">
                 </div>
 
                 <div class="Auth-inputContent">
                     <p class="Auth-label">メールアドレス</p>
-                    <input type="email" name="email" class="Auth-email Auth-signUpInput" id="Auth-signUp-mail" size="30">
+                    <input type="email" name="signUpEmail" class="Auth-email Auth-signUpInput" id="Auth-signUp-mail" size="30">
                 </div>
 
                 <div class="Auth-inputContent">
                     <p class="Auth-label">パスワード</p>
-                    <input type="password" name="password" class="Auth-password Auth-signUpInput" id="Auth-signUp-password" size="30" placeholder="半角英数8文字以上">
+                    <input type="password" name="signUpPassword" class="Auth-password Auth-signUpInput" id="Auth-signUp-password" size="30" placeholder="半角英数8文字以上">
                     <span class="far fa-eye" id="Auth-signUp-buttonEye" onclick="signUpHidePass()"></span>
                 </div>
 
                 <div class="Auth-inputContent">
+                    <p id="Auth-signUp-dbError">
+                        <?php
+                        if (isset($_SESSION['error'])) {
+                            echo $_SESSION['error'];
+                        }
+                        ?>
+                    </p>
                     <p class="Auth-signUpError" id="Auth-signUp-mailError"></p>
                     <p class="Auth-signUpError" id="Auth-signUp-passwordError"></p>
                 </div>
