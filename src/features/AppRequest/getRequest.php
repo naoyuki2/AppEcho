896d71b04d5c61ceaa8f366162a5c11cde6c29a1
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2024-06-19 09:52:33 +0900

    申請一覧取得(getRequestの作成)

---
+++ src/features/AppRequest/getRequest.php
@@ -0,0 +1,14 @@
+<?php
+require_once dirname(__FILE__, 4) . '/config/db_connect.php';
+
+function getRequest()
+{
+    global $pdo;
+    $sql = $pdo->query('
+            SELECT request.id, request.user_id, request.app_name, request.status, user.name AS user_name, user.icon_image_url AS user_icon
+            FROM request
+            INNER JOIN user
+            ON request.user_id = user.id
+        ');
+    return $sql->fetchAll();
+}
---
+++ src/pages/admin/RequestList.php
@@ -0,0 +1,15 @@
+<?php
+require_once dirname(__FILE__, 3) . '/components/Header.php';
+require_once dirname(__FILE__, 3) . '/features/AppRequest/getRequest.php';
+$allRequest = getRequest();
+?>
+
+<?php foreach ($allRequest as $request) { ?>
+    <div class="Request-box">
+        <img src="<?php echo $request['user_icon'] ?>">
+        <span class="Request-user-name"><?php echo $request['user_name'] ?></span>
+        <span class="Request-app-name"><?php echo $request['app_name'] ?></span>
+        <button class="Request-dest" value=2>破棄</button>
+        <button class="Request-accept" value=1>受理</button>
+    </div>
+<?php } ?>
