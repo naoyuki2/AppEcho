93f1b44d0102b05417f451966243be2811fece22
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2024-06-24 10:03:43 +0900

    破棄のロジックの実装

--- src/pages/admin/RequestList.php
+++ src/pages/admin/RequestList.php
@@ -7,16 +7,18 @@ $allRequest = getRequest();
 <?php foreach ($allRequest as $request) { ?>
     <div class="Request-box">
         <div class="Request-user-info">
-            <img src="<?php echo $request['user_icon'] ?>">
+            <img src="<?php echo $request['user_icon'] ?>" class="Request-user-icon">
             <span class="Request-user-name"><?php echo $request['user_name'] ?></span>
             <span class="Request-app-name"><?php echo $request['app_name'] ?></span>
         </div>
         <div class="Request-button">
-            <button class="Request-dest" value=2>破棄</button>
+            <button class="Request-dest" value=2 onclick="RequestDest(<?php echo $request['id'] ?>)">破棄</button>
             <button class="Request-accept" value=1>受理</button>
         </div>
     </div>
 <?php } ?>
+
+<script src="../../features/AppRequest/AppRequest.js"></script>
 
 <?php
     require_once dirname(__FILE__, 3) . '/components/Footer.php';
