25ff91e0cc783b9a7d7b9439a4bd224a084d2ede
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2024-06-24 11:33:05 +0900

    受理のロジックを実装

--- src/features/AppRequest/AppRequest.js
+++ src/features/AppRequest/AppRequest.js
@@ -1,4 +1,9 @@
 function RequestDest(id){
     confirm("本当に破棄してもよろしいですか？");
-    location.href="../../features/AppRequest/UpdateRequest.php?reqId="+id;
+    location.href="../../features/AppRequest/UpdateRequest.php?reqId="+id+"&judge=dest";
+}
+
+function RequestAccept(id){
+    confirm("本当に受理してもよろしいですか？");
+    location.href="../../features/AppRequest/UpdateRequest.php?reqId="+id+"&judge=accept";
 }
