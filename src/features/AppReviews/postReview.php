228864870d2a995296065ac939f528f02e9e9cdb
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-06-26 10:00:26 +0900

    postReviewの更新

--- src/features/AppReviews/postReview.php
+++ src/features/AppReviews/postReview.php
@@ -8,19 +8,20 @@ $appId = $_GET['appId'];
 $tagId = $_POST['tagId'];
 $content = $_POST['content'];
 $star = $_POST['star'];
+$userId = $_POST['userId'];
 
-postReviews($appId, $tagId, $content, $star);
+postReviews($appId, $tagId, $content, $star, $userId);
 header('Location: ../../pages/user/AppReviews.php?appId='.$appId);
 exit();
 
-function postReviews($appId, $tagId, $content, $star) {
+function postReviews($appId, $tagId, $content, $star, $userId) {
     try {
         global $pdo;
         $sql = $pdo->prepare('
-            INSERT INTO review(app_id, tag_id, content, star)
-            VALUES (?,?,?,?)
+            INSERT INTO review(app_id, tag_id, content, star, user_id)
+            VALUES (?,?,?,?,?)
         ');
-        $result = $sql->execute([$appId, $tagId, $content, $star]);
+        $result = $sql->execute([$appId, $tagId, $content, $star, $userId]);
 
         if (!$result) {
             echo "Insert failed!";
